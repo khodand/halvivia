b3620315453b9ae26dfad49ba53036098312d23c
Artem Kim <6aremkim@gmail.com>
2026-09-20 20:12:16 +0300

    fix: ask login to check activities

--- src/features/ActivityFeed/ActivityFeed.tsx
+++ src/features/ActivityFeed/ActivityFeed.tsx
@@ -1,11 +1,26 @@
 import { getRecentActivity } from '@/entities/activity/api/db';
 import { ActivityItem } from '@/features/ActivityFeed/ActivityItem';
+import { verifySession } from '@/shared/lib/auth';
+import { ROUTES } from '@/shared/config';
+import Link from 'next/link';
 
 type ActivityFeedProps = {
   limit?: number;
 };
 
 export async function ActivityFeed({ limit = 25 }: ActivityFeedProps) {
+  const session = await verifySession();
+
+  if (session.status === 'unauthenticated')
+    return (
+      <div className="text-muted-foreground px-4 py-12 text-center text-sm">
+        <Link className={'text-text-primary'} href={ROUTES.LOGIN}>
+          Войдите
+        </Link>
+        , чтобы посмотреть последнюю активность
+      </div>
+    );
+
   const events = await getRecentActivity(limit);
 
   if (events.length === 0) {
